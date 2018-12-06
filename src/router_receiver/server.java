667f5f362580bcc949706e33e8f3ac92068e03e5
kant1724 <sonseungbum@naver.com>
2018-12-06 11:25:06 +0900

    commit

--- src/router_receiver/server.java
+++ src/router_receiver/server.java
@@ -2,88 +2,46 @@ package router_receiver;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.net.StandardProtocolFamily;
 import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 import java.util.Iterator;
 
-public class server implements Runnable{
-	private final int port;
-	private ServerSocketChannel ssc;
-	private Selector selector;
-	private ByteBuffer buf = ByteBuffer.allocate(256);
-	server(int port) throws IOException {
-		this.port = port;
-		this.ssc = ServerSocketChannel.open();
-		this.ssc.socket().bind(new InetSocketAddress(port));
-		this.ssc.configureBlocking(false);
-		this.selector = Selector.open();
-
-		this.ssc.register(selector, SelectionKey.OP_ACCEPT);
-	}
-	
+public class server extends Thread {
 	public static void main(String[] args) throws IOException {
-		server server = new server(10523);
-		(new Thread(server)).start();
-	}
-	
-	@Override public void run() {
-		try {
-			System.out.println("Server starting on port " + this.port);
-
-			Iterator<SelectionKey> iter;
-			SelectionKey key;
-			while(this.ssc.isOpen()) {
-				selector.select();
-				iter=this.selector.selectedKeys().iterator();
-				while(iter.hasNext()) {
-					key = iter.next();
-					iter.remove();
-					if(key.isAcceptable()) this.handleAccept(key);
-					if(key.isReadable()) this.handleRead(key);
-				}
-			}
-		} catch(IOException e) {
-			System.out.println("IOException, server of port " +this.port+ " terminating. Stack trace:");
-			e.printStackTrace();
-		}
-	}
-	private final ByteBuffer welcomeBuf = ByteBuffer.wrap("Welcome to NioChat!\n".getBytes());
-
-	private void handleAccept(SelectionKey key) throws IOException {
-		SocketChannel sc = ((ServerSocketChannel) key.channel()).accept();
-		String address = (new StringBuilder( sc.socket().getInetAddress().toString() )).append(":").append( sc.socket().getPort() ).toString();
-		sc.configureBlocking(false);
-		sc.register(selector, SelectionKey.OP_READ, address);
-		sc.write(welcomeBuf);
-		welcomeBuf.rewind();
-		System.out.println("accepted connection from: "+address);
-	}
-
-	private void handleRead(SelectionKey key) throws IOException {
-		SocketChannel ch = (SocketChannel) key.channel();
-		StringBuilder sb = new StringBuilder();
-
-		buf.clear();
-		int read = 0;
-		while( (read = ch.read(buf)) > 0 ) {
-			buf.flip();
-			byte[] bytes = new byte[buf.limit()];
-			buf.get(bytes);
-			sb.append(new String(bytes));
-			buf.clear();
-		}
-		String msg;
-		if(read<0) {
-			msg = key.attachment()+" left the chat.\n";
-			ch.close();
-		}
-		else {
-			msg = key.attachment()+": "+sb.toString();
-		}
-
-		System.out.println(msg);
+        DatagramChannel datagramChannel = DatagramChannel.open(StandardProtocolFamily.INET);
+        datagramChannel.bind(new InetSocketAddress(10523));
+ 
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("[수신 시작]");
+ 
+                try {
+                    while (true) {
+                        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(100);
+                        SocketAddress socketAddress = datagramChannel.receive(byteBuffer);
+                        byteBuffer.flip();
+ 
+                        Charset charset = Charset.forName("UTF-8");
+                        String data = charset.decode(byteBuffer).toString();
+                        System.out.println("[받은 내용: " + socketAddress.toString() + "] " + data);
+                    }
+                } catch (Exception e) {
+                    System.out.println("[수신 종료]");
+                }
+            }
+        };
+        thread.start();
+        try {
+        	Thread.sleep(10000);
+        } catch (Exception e) {}
+        datagramChannel.close();
 	}
 }
