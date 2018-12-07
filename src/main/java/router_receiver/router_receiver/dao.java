6a91eabd57df6f9d22a314349c89ffdebef924e6
kant1724 <sonseungbum@naver.com>
2018-12-07 17:01:58 +0900

    commit

--- src/main/java/router_receiver/router_receiver/dao.java
+++ src/main/java/router_receiver/router_receiver/dao.java
@@ -4,9 +4,11 @@ import static com.mongodb.client.model.Filters.eq;
 
 import org.bson.Document;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 
@@ -15,7 +17,12 @@ public class dao {
 		MongoClient mongoClient = MongoClients.create("mongodb://14.63.168.58:27017");
 		MongoDatabase database = mongoClient.getDatabase("router_log");
 		MongoCollection<Document> collection = database.getCollection("router_log");
-		collection.find(eq("1", "a"));
+		FindIterable<Document> doc = collection.find(eq("1", "a"));
+		MongoCursor<Document> cursor = doc.iterator();
+		while (cursor.hasNext()) {
+			String res = cursor.next().toJson();
+			System.out.println(res);
+		}
 	}
 	
 	public static void main(String[] args) {
